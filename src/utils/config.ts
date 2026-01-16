30bd32b0df1344936d7c1d467f1893e8f534f123
Ford Pendleton <FPendleton@tcf.com>
2026-01-16 13:36:02 -0600

    Add fallback API key for GitHub Pages

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,7 @@
-export const TMDB_API_BASE_URL: string | undefined = import.meta.env
-  .VITE_TMDB_API_BASE_URL;
-export const API_KEY: string | undefined = import.meta.env.VITE_API_KEY;
+export const TMDB_API_BASE_URL: string =
+  import.meta.env.VITE_TMDB_API_BASE_URL || "https://api.themoviedb.org/3";
+export const API_KEY: string =
+  import.meta.env.VITE_API_KEY || "fb4a2cf9cd8004e46596605a7f8cc93d";
 
 export const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
 export const GOOGLE_AD_SLOT=import.meta.env.VITE_GOOGLE_AD_SLOT;
